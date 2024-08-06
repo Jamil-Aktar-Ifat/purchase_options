c7e778d6e74da34bc27c3db32c4186837dfc5e08
ifat <i>
2024-08-06 21:23:53 +0100

    responsive menu btn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
+import { useState } from "react";
+import "./App.css";
+import DaisyNavBar from "./components/DaisyNavBar/DaisyNavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <NavBar></NavBar>
-      <h1 className='text-2xl'>Vite + React</h1>
-      
+      <NavBar></NavBar>
+      <DaisyNavBar></DaisyNavBar>
+      <h1 className="text-2xl">Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,23 @@
 import Link from "../Link/Link";
+import { RiMenu5Line } from "react-icons/ri";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
 
-const routes = [
-  { id: 1, path: "/", name: "Home" },
-  { id: 2, path: "/about", name: "About" },
-  { id: 3, path: "/contact", name: "Contact" },
-  { id: 4, path: "/profile", name: "Profile" },
-  { id: 5, path: "/dashboard", name: "Dashboard" },
-];
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/contact", name: "Contact" },
+    { id: 4, path: "/profile", name: "Profile" },
+    { id: 5, path: "/dashboard", name: "Dashboard" },
+  ];
   return (
     <nav>
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {open === true ? <RxCross1 /> : <RiMenu5Line />}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
