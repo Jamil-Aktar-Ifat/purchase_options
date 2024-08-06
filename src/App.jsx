402e70023ecf95673a67cdef3121084678b6db7d
ifat <i>
2024-08-06 20:53:59 +0100

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <NavBar></NavBar>
       <h1 className='text-2xl'>Vite + React</h1>
       
     </>
